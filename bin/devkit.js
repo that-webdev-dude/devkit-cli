a1958c0207a5a0d81ffef532f46e0094426a76ae
that-webdev-dude <that.webdev.dude@gmail.com>
2021-12-16 23:01:18 +0000

    install functions

--- bin/devkit.js
+++ bin/devkit.js
@@ -1,27 +1,5 @@
 #!/usr/bin/env node
 
-// const program = require('commander');
-// const { version } = require('../package.json');
-// const { create } = require('../lib/');
-
-// program.version(version).description('DevKit CLI');
-
-// program
-//   .command('create <projectName>')
-//   .description('create project')
-//   .action(async (projectName) => {
-//     try {
-//       await create(projectName);
-//       // notify.success(`project ${projectName} created successfully!\n`);
-//       // notify.info(`navigate to ${projectName}/client`);
-//       // notify.info(`start the dev server: npm run dev:client`);
-//       // notify.info(`build for deployment: npm run build`);
-//     } catch (error) {
-//       // notify.failure(error.message);
-//       console.log(error);
-//     }
-//   });
-
 import program from 'commander';
 import { create } from '../lib/index.js';
 import { readFile } from 'fs/promises';
